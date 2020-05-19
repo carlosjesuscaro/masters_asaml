2b058df60db054afba5703a7b7131d2a87a5a3f3
carloscaro <carlosjesuscaro@icloud.com>
2020-05-19 14:45:38 -0400

    Linear regression

--- class 1.R
+++ class 1.R
@@ -4,4 +4,8 @@ y = 3 - 2*x + rnorm(100)
 
 # Plotting
 plot(x,y)
+title(main='Linear Regression Example',xlab = 'Uniform points',ylab = 'Linear EQ w/ noise')
 
+# Linear regression
+L = lm(x~y)
+sum(L)
