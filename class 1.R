76b36382aaffb6a73dd6f91d8589a38f9fb73b65
carloscaro <carlosjesuscaro@icloud.com>
2020-05-19 15:50:27 -0400

    Checking the outcome

--- class 1.R
+++ class 1.R
@@ -7,5 +7,25 @@ plot(x,y)
 title(main='Linear Regression Example',xlab = 'Uniform points',ylab = 'Linear EQ w/ noise')
 
 # Linear regression
-L = lm(x~y)
-sum(L)
+L = lm(y~x)
+summary(L)
+names(L)
+L$coefficients
+L$rank
+L$call
+L$model
+sigmahatn2 = sum(L$residuals^2)/98
+sigmahatn2
+sqrt(sigmahatn2)
+summary(L)
+
+# Checking that the noise is Gaussian (through the residuals)
+R = L$residuals
+hist(R,freq = FALSE) 
+plot(L) 
+# one of the graphs shows with a red line the expectation of the noise (zero) and the points around the line.
+# If the number of points on each side of the red line is equal, then we can accept the solution (concept of 
+# homoscedasticity). It is important to check that we have the same variance across the dataset
+
+
+ 
