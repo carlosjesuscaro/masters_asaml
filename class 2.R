ddb029ca888fe0832680ffcbdad10a6e64bd684c
carloscaro <carlosjesuscaro@icloud.com>
2020-05-28 03:26:14 -0400

    New prediction w/ new data

--- class 2.R
+++ class 2.R
@@ -1,13 +1,13 @@
 # Defining the equation
 x = runif(50,-7,7)
-y = 5-6*x+rnorm(50)
+y = 5-6*x+rnorm(50,0,50)
 # Ploting the equation
 plot(x,y)
 title('Linear equation')
 grid()
 # Linear model
 L = lm(y~x)
-L
+summary(L)
 # the output is the coefficienties: (Intercept -> a0hat)/ (x -> a1hat
 
 # L$coefficients: exact values
@@ -24,6 +24,12 @@ predict(L)
 new = data.frame(x = seq(-3,3,0.1))
 
 # Obtaining the confidence interval and prediction
+# The prediction always require a new data set (new observation)
 pred.w.plim <- predict(L, new, interval = "prediction")
+# The confidence does not always require a new data set E[y]
 pred.w.clim <- predict(L, new, interval = "confidence")
+#matplot(new$x, cbind(pred.w.clim, pred.w.plim[,-1]), lty = c(1,2,2,3,3), type = "l", ylab = "predicted y")
+grid()
+# Plotting individually:
 matplot(new$x, cbind(pred.w.clim, pred.w.plim[,-1]), lty = c(1,2,2,3,3), type = "l", ylab = "predicted y")
+grid()
