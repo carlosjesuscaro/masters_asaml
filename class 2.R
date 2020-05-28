52d9f62cc883cf09272ec027a5e350a51c2de70e
carloscaro <carlosjesuscaro@icloud.com>
2020-05-28 02:10:10 -0400

    New prediction w/ new data

--- class 2.R
+++ class 2.R
@@ -2,18 +2,28 @@
 x = runif(50,-7,7)
 y = 5-6*x+rnorm(50)
 # Ploting the equation
-plot(x,y,)
+plot(x,y)
 title('Linear equation')
 grid()
 # Linear model
 L = lm(y~x)
 L
-# the output is the coefficienties:
-# (Intercept -> a0hat)
-# (x -> a1hat
+# the output is the coefficienties: (Intercept -> a0hat)/ (x -> a1hat
 
 # L$coefficients: exact values
 L$coefficients
 abline(L,col='red')
 
-help(predict.lm())
+# Calling for help in R
+#help(predict.lm)
+
+# Prediction of y_hat associated to the training sample
+# Prediction associated to a new observaton
+predict(L)
+# Creating new data set
+new = data.frame(x = seq(-3,3,0.1))
+
+# Obtaining the confidence interval and prediction
+pred.w.plim <- predict(L, new, interval = "prediction")
+pred.w.clim <- predict(L, new, interval = "confidence")
+matplot(new$x, cbind(pred.w.clim, pred.w.plim[,-1]), lty = c(1,2,2,3,3), type = "l", ylab = "predicted y")
