68e773d656a7002105b3783149f1b9b9a0f0a520
carloscaro <carlosjesuscaro@icloud.com>
2020-05-27 21:49:08 -0400

    Plotting and linear regression

--- class 2.R
+++ class 2.R
@@ -1,5 +1,19 @@
 # Defining the equation
 x = runif(50,-7,7)
-y = 5-6*6+rnorm(50)
+y = 5-6*x+rnorm(50)
 # Ploting the equation
-plot(x, y,title='Class 2')
+plot(x,y,)
+title('Linear equation')
+grid()
+# Linear model
+L = lm(y~x)
+L
+# the output is the coefficienties:
+# (Intercept -> a0hat)
+# (x -> a1hat
+
+# L$coefficients: exact values
+L$coefficients
+abline(L,col='red')
+
+predict()
