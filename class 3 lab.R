d1c98ac2a0aabc3b7374de76b08cbd2453c09f13
Carlos Caro <carlosjesuscaro@icloud.com>
2020-05-30 20:08:03 -0400

    Uploading the data sets

---
+++ class 3 lab.R
@@ -0,0 +1,16 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: carloscaro
+# Created on: 2020-05-30
+
+# Uploading the data
+Y1 = read.table('Y1.txt')
+Y2 = read.table('2.txt')
+Y3 = read.table('Y3.txt')
+Y4 = read.table('Y4.txt')
+A1 = read.table('A1.txt')
+A3 = read.table('A3.txt')
+A4 = read.table('A4.txt')
+
+
+
