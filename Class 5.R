9271b884664c20a08032e9683e644123ef2b3292
carloscaro <carlosjesuscaro@icloud.com>
2020-06-07 19:55:34 -0400

    setup

---
+++ Class 5.R
@@ -0,0 +1,12 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: carloscaro
+# Created on: 2020-06-07
+
+# Loading the data
+ozone <- read.table('ozone.txt')
+head(ozone)
+dim(ozone)
+# we have 112 samples and 13 variables
+
+#
