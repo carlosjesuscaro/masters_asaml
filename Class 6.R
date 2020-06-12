1ef520e043c1e82e2c1bd3f092695717818d6a0f
Carlos Caro <carlosjesuscaro@icloud.com>
2020-06-12 05:44:50 -0400

    Basic examples

--- Class 6.R
+++ Class 6.R
@@ -5,9 +5,24 @@
 
 # Loading the library associated with CART
 library(rpart)
+# Classification example
 data(iris)
 Tree1 = rpart(iris$Species~.,data=iris[,-5])
+plot(Tree1)
 # This is a more proper tree
 Tree2 = rpart(iris$Species~.,data=iris[,-5],
               control = rpart.control(cp=10^-9,
               minsplit = 1))
+plot(Tree2)
+# cp: complex parameter
+# minsplit: minimum number of observations that must exist in
+# a node in order for a split to be attempted
+
+# Regression example
+data(mtcars)
+Tree3 = rpart(mtcars$mpg~.,data=mtcars[,-1])
+plot(Tree3)
+Tree4 = rpart(mtcars$mpg~.,data=mtcars[,-1],
+              control = rpart.control(cp=10^-9,
+                                      minsplit = 2))
+plot(Tree4)
