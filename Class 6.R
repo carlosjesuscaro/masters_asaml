ccce3afcbdf669c8a8283ae1bd3515ff532fd4b3
Carlos Caro <carlosjesuscaro@icloud.com>
2020-06-14 21:23:18 -0400

    Basic examples v2.0

--- Class 6.R
+++ Class 6.R
@@ -9,11 +9,13 @@ library(rpart)
 data(iris)
 Tree1 = rpart(iris$Species~.,data=iris[,-5])
 plot(Tree1)
+plotcp(Tree1)
 # This is a more proper tree
 Tree2 = rpart(iris$Species~.,data=iris[,-5],
               control = rpart.control(cp=10^-9,
               minsplit = 1))
 plot(Tree2)
+plotcp(Tree2)
 # cp: complex parameter
 # minsplit: minimum number of observations that must exist in
 # a node in order for a split to be attempted
@@ -22,7 +24,12 @@ plot(Tree2)
 data(mtcars)
 Tree3 = rpart(mtcars$mpg~.,data=mtcars[,-1])
 plot(Tree3)
+plotcp(Tree3)
 Tree4 = rpart(mtcars$mpg~.,data=mtcars[,-1],
               control = rpart.control(cp=10^-9,
                                       minsplit = 2))
 plot(Tree4)
+plotcp(Tree4)
+printcp(Tree4)
+summary(Tree4 )
+
