7eb4b978e315e2a9357be2fa418ab97397239a23
carloscaro <carlosjesuscaro@icloud.com>
2021-01-20 23:31:56 -0500

    ARBRE dataset analysis

--- Class 5b.R
+++ Class 5b.R
@@ -6,3 +6,18 @@
 ble <- read.table('Dataset_Ble.txt', header = TRUE, sep = ";")
 
 load('arbre.RData')
+# Assuming that we comply with all the basic conditions, we
+# can proceed with building the model
+
+L = lm(arbre$hauteur~arbre$hetraie, data=arbre)
+anova(L)
+# based om the anova result, we can conclude that hetrai has an effect on
+# heuteu
+summary(L)
+# based on the summary, we see that hetraie2 has a high p-value which
+# means that we keep H0 so we understand that hetraie2 is equal to hetraie1
+pairwise.t.test(arbre$hauteur,arbre$hetraie,p.adjust="bonf")
+# this confirms that level 1 and 2 are the same and have the same effect
+
+#Comparing by pairs:
+TukeyHSD(aov((arbre$hauteur~arbre$hetraie)))
